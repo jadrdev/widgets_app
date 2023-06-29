d8bd4f88e5a96983e28397cf42843e3c525d4ffb
Joshua <jadrdevcm@gmail.com>
2023-06-29 18:51:13 +0100

    Home Screen Implementacion

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,42 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final item = appMenuItems[index];
+          return _CustomListTitle(item: item);
+        });
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({required this.item});
+
+  final MenuItems item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(item.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios, color: colors.primary),
+      title: Text(item.title),
+      subtitle: Text(item.subtitle),
+      onTap: () => {
+        // TODO: Navigate to the selected screen
+      },
     );
   }
 }
