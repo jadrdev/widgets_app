bc01c88558810378e83b6f8b6ae6d349fa7e4ba9
Joshua A. Díaz Robayna <jrobayna@velocitech.io>
2023-07-19 11:04:26 +0100

    Screen Creadas y Barril Actualizado

---
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PogressScreen extends StatelessWidget {
+  static const name = 'progress_screen';
+  const PogressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
